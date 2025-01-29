cb2ac7f61ce52242adb786dac4eb70fce17e7b7d
Andreas Copan <avcopan@gmail.com>
2025-01-29 13:47:01 -0500

    New: Completes Project 1 background/instructions

---
+++ projects/02_hessian_draft/util.py
@@ -0,0 +1,14 @@
+"""Utility functions for Project 1"""
+
+
+def example_function(name: str = "you") -> str:
+    """Generate a string with a warm greeting.
+
+    This is an example function, documented in the Sphinx docstring format.
+
+    See here: https://sphinx-rtd-tutorial.readthedocs.io/en/latest/docstrings.html#the-sphinx-docstring-format
+
+    :param name: Name for greeting
+    :return: Greeting
+    """
+    return f"Hello, {name}!"
